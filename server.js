01fd3a593a96a9a85f872947765613c152f8694d
lishihai <jpweiyi@163.com>
2016-12-03 01:07:28 +0800

    5

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
-var http = require("http");
-http.createServer(function(request, response) {  
-    response.writeHead(200, {"Content-Type": "text/plain"});  
-    response.write("Hello World");  
-    response.end();
-}).listen(80);
-console.log("nodejs start listen 8080 port!");
+var express = require('express');
+var AV = require('leanengine');
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID || 'A4gzB4MkJ2hE5DlRU9eJaFKl-9Nh9j0Va',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'uaXPOvBHVJlDmbl6aAGCN0n0',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'h7U1NhMTezg6vixpQP7un7Dm'
+});
+
+var app = express();
+app.use(AV.express());
+app.listen(process.env.LEANCLOUD_APP_PORT);
+
+app.get('/', function(req, res) {
+  res.render('index', {title: 'Hello world'});
+});
+
+app.get('/time', function(req, res) {
+  res.json({
+    time: new Date()
+  });
+});
+
+app.get('/todos', function(req, res) {
+  new AV.Query('Todo').find().then(function(todos) {
+    res.json(todos);
+  }).catch(function(err) {
+    res.status(500).json({
+      error: err.message
+    });
+  });
+});
